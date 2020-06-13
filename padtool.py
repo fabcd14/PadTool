7d6d74359d846a1ac1cd83aba7816d22237029b8
Fabien Cuny <fabien.cuny7@orange.fr>
2020-06-13 19:05:19 +0200

    Release v0.9.7

--- padtool.py
+++ padtool.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with PadTool.  If not, see <http://www.gnu.org/licenses/>.
 
-version = "v0.9.6"
+version = "v0.9.7"
 
 # Import system libraries
 
